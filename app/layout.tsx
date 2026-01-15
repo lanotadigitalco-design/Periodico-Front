d7b739aacee7ff6824c996a57784fe9dcc8628b6
bitorham <hernandotorralvo@gmail.com>
2026-01-15 17:47:55 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,15 +15,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/favicon.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/favicon.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/favicon.png",
         type: "image/svg+xml",
       },
     ],
