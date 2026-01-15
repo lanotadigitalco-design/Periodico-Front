ade9ecd264b7c8e58d3dd44a6bdcdf0fb259ed19
bitorham <hernandotorralvo@gmail.com>
2026-01-15 16:27:10 -0500

    Update logo-section.tsx

--- components/logo-section.tsx
+++ components/logo-section.tsx
@@ -6,7 +6,7 @@ import Image from "next/image"
 export function LogoSection() {
   return (
     <Link href="/" className="flex items-center justify-center flex-shrink-0">
-      <Image src="/logo.png" alt="La Nota Digital" width={500} height={100} className="h-20 md:h-32 w-auto" priority />
+      <Image src="/logo.png" alt="La Nota Digital" width={500} height={50} className="h-8 md:h-12 w-auto" priority />
     </Link>
   )
 }
