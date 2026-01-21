cfa15655541967e1fb555e913f5f2b274a36d408
bitorham <hernandotorralvo@gmail.com>
2026-01-21 15:52:37 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PDCZYWMEZW"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PDCZYWMEZW');
+            `,
+          }}
+        />
+      </head>
+
       <body className={`font-sans antialiased`}>
         <AuthProvider>
           <div className="relative border-b border-border bg-card sticky top-0 z-50">
